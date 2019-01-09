3964c29b14950ead9750ded4e23aa8c9b0cac13d
Claudine5 <45870457+Claudine5@users.noreply.github.com>
2019-01-09 12:58:19 +0100

    Add files via upload

--- contour.py
+++ contour.py
@@ -109,11 +109,14 @@ def masqueSansQueue(segmentation):
     masque2 = cv2.morphologyEx(masque2, cv2.MORPH_CLOSE, kernel)
     # On seuille le masque
     ret, masquethresh = cv2.threshold(masque2, 127, 255, 0)
+
     return masquethresh
 
 #Fonction pour determiner si la feuille est en forme de triangle
+#TODO : ATTENTION ca ne marche que pour les feuille convexe car on utilie le vrai contour de la feuille pas le contour convexe
 def checkTriangle(masqueSansQueue, input):
     compteurdetriangle = 0
+
     height, width, channels = input.shape
 
     # On recupere le contour du masque
@@ -124,11 +127,12 @@ def checkTriangle(masqueSansQueue, input):
     # cv2.drawContours(input, contourUtile, -1, (255, 0, 0), 2)
     # cv2.imshow("contour utilise", input)
 
-    #On determine le 'centre' de la feuille
+    # On determine le 'centre' de la feuille
     M = cv2.moments(hull)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     # cv2.circle(input, (cX, cY), 5, [0, 2, 255], -1)
+
 
 
     #Avec le contour convex on calcul les angle en chaque point :
@@ -139,89 +143,107 @@ def checkTriangle(masqueSansQueue, input):
         c = hull[i + 1][0]
         angle = calcAngle(a, b, c)
         listeAngle = np.append(listeAngle, angle)
-    # calcul de l'angle : avant-dernier,dernier,premier point du contour
+        # calcul de l'angle : avant-dernier,dernier,premier point du contour
     a = hull[len(hull) - 2][0]
     b = hull[len(hull) - 1][0]
     c = hull[0][0]
     angle = calcAngle(a, b, c)
     listeAngle = np.append(listeAngle, angle)
 
-    #Liste des points pour lesquels l'angle est faible (<140)
+    # Liste des points pour lesquels l'angle est faible (<140)
     listeAngleFaible = []
     for i in range(len(listeAngle)):
         if listeAngle[i] < 140:
             # cv2.circle(input, tuple(hull[i][0]), 5, [0, 2, 255], -1)
             listeAngleFaible.append(hull[i][0])
-        # cv2.imshow("sommets possible detectes", input)
+            # cv2.imshow("sommets possible detectes", input)
     # print("il y a {} sommets detectes".format(len(listeAngleFaible)))
 
     # On retire les sommets trop proches
-    aRetirer = [] #liste qui contient les indices des elements a retirer
+    aRetirer = []  # liste qui contient les indices des elements a retirer
     if len(listeAngleFaible) > 1:
         for i in range(len(listeAngleFaible)):
             if dist(listeAngleFaible[i - 1], listeAngleFaible[i]) < 20:
                 # retirer un des points proches
-                if (i-1 not in aRetirer):
+                if (i - 1 not in aRetirer):
                     aRetirer.append(i)
     aRetirer.reverse()
     for i in range(len(aRetirer)):
         del listeAngleFaible[aRetirer[i]]
-    # print("il y a {} sommets restants".format(len(listeAngleFaible)))
+        # print("il y a {} sommets restants".format(len(listeAngleFaible)))
+
 
     for i in range(len(listeAngleFaible)):
         pointAngle = listeAngleFaible[i]
+        pointAngle = [float(pointAngle[0]), float(pointAngle[1])]
         # calcul du vecteur entre le sommet et le centre
         vecSommetCentre = [cX - pointAngle[0], cY - pointAngle[1]]
         vecPerpendiculaire = [-vecSommetCentre[1], vecSommetCentre[0]]
 
         #Choix de 2 points base et sommet pour calculer la largeur a ces 2 endroits
         #Si le rapport est assez grand on considere qu'on a un triangle
-        base = [cX+int(0.6*vecSommetCentre[0]), cY+int(0.6*vecSommetCentre[1])]
-        sommet = [pointAngle[0]+int(0.3*vecSommetCentre[0]), pointAngle[1]+int(0.3*vecSommetCentre[1])]
 
-        # cv2.circle(input, tuple(base), 5, [255,0, 255], -1)
-        # cv2.circle(input, tuple(sommet), 5, [255, 2, 255], -1)
+        pDepart, baseEnd = trouverPointContour(pointAngle, vecSommetCentre, 1, 0.01, 5, contourUtile, input)
+
+        # cv2.circle(input, tuple([int(baseEnd[0]), int(baseEnd[1])]), 5, [255, 0, 0], -1)
+        # cv2.circle(input, tuple([int(pointAngle[0]), int(pointAngle[1])]), 5, [0, 0, 255], -1)
+        vecSommetBase = [baseEnd[0]-pointAngle[0], baseEnd[1]-pointAngle[1]]
+        vbase = [0.9*vecSommetBase[0], 0.9*vecSommetBase[1]]
+        base = [pointAngle[0]+vbase[0], pointAngle[1]+vbase[1]]
+        vsommet = [0.1*vecSommetBase[0], 0.1*vecSommetBase[1]]
+        sommet = [pointAngle[0] + vsommet[0], pointAngle[1] + vsommet[1]]
+
+        # cv2.circle(input, tuple([int(base[0]),int(base[1])]), 5, [255,0, 255], -1)
+        # cv2.circle(input, tuple([int(sommet[0]),int(sommet[1])]), 5, [255, 2, 255], -1)
         # cv2.imshow("triangle", input)
 
 
         #pas avec lequel on parcours la perpendiculaire a la recherche de point du sommet dans cette direction
-        pas=0.01
+        pas=0.02
         #distance max pour conciderer qu'on est assez proche d'un point
-        seuil = 10
+        seuil = 7
         pDepart, sommetGauche = trouverPointContour(sommet, vecPerpendiculaire, 1, pas, seuil, contourUtile, input)
+
         # print("fin triangle 1/4")
 
-        pDepart, sommetDroit = trouverPointContour(pDepart, vecPerpendiculaire, -1, pas, seuil, contourUtile, input)
+        x, sommetDroit = trouverPointContour(sommet, vecPerpendiculaire, -1, pas, seuil, contourUtile, input)
+
         # print("fin triangle 2/4")
 
-        pDepart, baseGauche = trouverPointContour(base, vecPerpendiculaire, 1, pas, seuil, contourUtile, input)
+        x, baseGauche = trouverPointContour(base, vecPerpendiculaire, 1, pas, seuil, contourUtile, input)
+
         # print("fin triangle 3/4")
 
-        pDepart, baseDroit = trouverPointContour(pDepart, vecPerpendiculaire, -1, pas, seuil, contourUtile, input)
+        x, baseDroit = trouverPointContour(base, vecPerpendiculaire, -1, pas, seuil, contourUtile, input)
+
         # print("fin triangle 4/4")
 
         rapport = dist(baseDroit, baseGauche) / dist(sommetDroit, sommetGauche)
         print("rapport = {}".format(rapport))
-        if rapport>1.8 :
+        if rapport>2.7 :
             compteurdetriangle += 1
             # cv2.circle(input, tuple(pointAngle), 5, [255, 255, 0], -1)
 
-        # cv2.imshow("triangle", input)
+        cv2.imshow("triangle", input)
     return compteurdetriangle
 
 def trouverPointContour(pDepart, vecteur, direction, pas, seuil, contour, input):
+
     height, width, channels = input.shape
     enCours = True
     baseD = copy.deepcopy(pDepart)
-    baseD[0] += direction * 0.1 * vecteur[0]
-    baseD[1] += direction * 0.1 * vecteur[1]
+    pDepart2 = copy.deepcopy(pDepart)
+    baseD[0] += direction * 0.15 * vecteur[0]
+    baseD[1] += direction * 0.15 * vecteur[1]
+    # cv2.circle(input, tuple([int(baseD[0]), int(baseD[1])]), 1, [255, 255, 0], -1)
     while (enCours):
         baseD[0] += direction * pas * vecteur[0]
         baseD[1] += direction * pas * vecteur[1]
+
         if (baseD[0] < 0 or baseD[0] > width) or (baseD[1] < 0 or baseD[1] > height):
-            pDepart[0] += -1*direction * 15 * pas * vecteur[0]
-            pDepart[1] += -1 * direction * 15 * pas * vecteur[1]
-            baseD = copy.deepcopy(pDepart)
+            pDepart2[0] += -1*direction * 10 * pas * vecteur[0]
+            pDepart2[1] += -1 * direction * 10 * pas * vecteur[1]
+            baseD = copy.deepcopy(pDepart2)
         else:
             for j in range(len(contour)):
                 pContour = contour[j][0]
@@ -229,7 +251,11 @@ def trouverPointContour(pDepart, vecteur, direction, pas, seuil, contour, input)
                 if d < seuil:
                     pointOutput = pContour
                     enCours = False
-    return (pDepart,pointOutput)
+    # cv2.circle(input, tuple([int(pointOutput[0]), int(pointOutput[1])]), 5, [255,255,255], -1)
+
+    return (pDepart2,pointOutput)
+
+
 
 def redimension(imageDeBase):
     # changer la taille d'une image :
@@ -277,12 +303,12 @@ def etude_classificateur(convexite, dents, jsonPath):
     return listeResultat
 
 def main():
-    # input = cv2.imread("base_donnee_feuille/hetre/hetre1.jpg")
+    input = cv2.imread("base_donnee_feuille/hetre/hetre1.jpg")
     # input = cv2.imread("base_donnee_feuille/hetre/hetre2.jpg")
     # input = cv2.imread("base_donnee_feuille/chene/chene2.jpg")
     # input = cv2.imread("base_donnee_feuille/margousier/margousier2.jpg")
     # input = cv2.imread("base_donnee_feuille/bouleau/bouleau1.jpg")
-    input = cv2.imread("base_donnee_feuille/bouleau/bouleau2.jpg")
+    # input = cv2.imread("base_donnee_feuille/bouleau/bouleau2.jpg")
     # input = cv2.imread("base_donnee_feuille/bouleau/bouleau3.jpg")
     # input = cv2.imread("base_donnee_feuille/platane/platane1.jpg")
 
@@ -313,19 +339,134 @@ def main():
     cv2.circle(input, (cX,cY), 5, [0, 2,255], -1)
     # cv2.imshow("centre", input)
 
-    ## Detection d'un cercle
+    ## Detection d'une elipse/rectangle
+
+
+    #methode cercle :
+    #la feuille est supposee symetrique
+    #On parcours la moitie des points et 1 point sur 3 (d'ou le /6 et le i+3)
+    #Pour chaque point on charge celui en face pa rapport au centre
+    #On mesure la distance entre les 2 points opposes
+    #On regarde si c'est constant
+    #(plus precis que regarder les distance centre-bord mais plus long a calculer)
+
+    listeDiametre = np.array([])
+    for i in range(int(len(contourUtile)/6)):
+        point = contourUtile[i+3][0]
+        pX, pY = point
+        vPointCentre = [cX - pX, cY - pY]
+        x, p2 = trouverPointContour([cX,cY], vPointCentre, 1, 0.02, 20, contourUtile, input)
+        d = dist(point, p2)
+        listeDiametre = np.append(listeDiametre, d)
+
+
+    print("ecart type des diametres : {}".format(listeDiametre.std()))
+    if (listeDiametre.std() < 15):  # 20 : valeur a ajuster
+        print("!!!! cercle")
+
+
+
+    #methode rectangle :
     #Calcul distance centre -> contour :
-    listeDistance = np.array([])
-    for i in range(len(hull)):
-        point = hull[i][0]
+    listeDistanceCentreContour = np.array([])
+    for i in range(len(contourUtile)):
+        point = contourUtile[i][0]
         pX, pY = point
         distance = ((cX-pX)**2+(cY-pY)**2)**0.5
-        listeDistance = np.append(listeDistance,distance)
+        listeDistanceCentreContour = np.append(listeDistanceCentreContour,distance)
 
-    print(listeDistance.std())
 
-    if (listeDistance.std() < 17 ): #17 : valeur a ajuster/diminuer
-        print("cercle")
+    #On cherche le grand axe
+    indiceGrandAxe1 = listeDistanceCentreContour.argmax() #le point le plus loin du centre
+    pointGrandAxe1 = contourUtile[indiceGrandAxe1][0]
+    cv2.circle(input, tuple(pointGrandAxe1), 7, [100,100,100], -1)
+
+    #le point de l'autre cote -> celui qui a la plus grande distance avec lui
+    dMax = -1
+    for i in range(len(contourUtile)):
+        point = contourUtile[i][0]
+        pX, pY = point
+        distance = dist(pointGrandAxe1, [pX,pY])
+        if distance > dMax :
+            dMax = distance
+            pointGrandAxe2 = contourUtile[i][0]
+
+    cv2.circle(input, tuple(pointGrandAxe2), 7, [100,100,100], -1)
+
+
+    vecGrandAxe = [pointGrandAxe2[0]-pointGrandAxe1[0], pointGrandAxe2[1]-pointGrandAxe1[1]]
+    vecPerpGrandAxe = [-vecGrandAxe[1], vecGrandAxe[0]]
+    pas, seuil = 0.01,10
+    listeDistPerp = np.array([])
+    #On parcours le grand axe et on mesure les longueurs sur a peu pres tout l'axe (on ignore les extremites
+    # car les feuilles rectangulaires le sont vers le centre)
+    #Si la feuille est entre 0 et 10, on va regarder entre 2 et 8 pour enlever la pointe
+    for i in range(0,41):
+        pourcent = 0.20 + i*0.015
+        pDepart = [float(pointGrandAxe1[0]) + pourcent * vecGrandAxe[0],
+                   float(pointGrandAxe1[1]) + pourcent * vecGrandAxe[1]]
+        x, sommetA = trouverPointContour(pDepart, vecPerpGrandAxe, 1, pas, seuil, contourUtile, input)
+        x, sommetB = trouverPointContour(pDepart, vecPerpGrandAxe, -1, pas, seuil, contourUtile, input)
+        d = dist(sommetA, sommetB)
+        listeDistPerp = np.append(listeDistPerp, d)
+
+    print("std liste des distance perp = {}".format(listeDistPerp.std()))
+    #Si les longueur perpendiculaires au grand axe sont 'constantes' c'est un rectangle
+    if listeDistPerp.std() <  20 :
+        print("!!!!! c'est un rectangle")
+
+
+    #Si ce n'est pas un rectangle : on regarde le demi grand axe et on regarde si c'est un carre (sinon on regarde si c'est ellipse)
+    #TODO : ENLEVER LES POINTS AUX EXTREMITE DU GRAND AXE POUR NE PAS LES PRENDRE EN COMPTE LORS DU CALCUL POUR ELLIPSE
+
+    #Demi grand axe : On va au centre du grand axe et on cherche les points
+    centreGrandAxe=[float(pointGrandAxe1[0]) + 0.5 * vecGrandAxe[0],
+                    float(pointGrandAxe1[1]) + 0.5 * vecGrandAxe[1]]
+    x, pointPetitAxe1 = trouverPointContour(centreGrandAxe, vecPerpGrandAxe, 1, pas, seuil, contourUtile, input)
+    x, pointPetitAxe2 = trouverPointContour(centreGrandAxe, vecPerpGrandAxe, -1, pas, seuil, contourUtile, input)
+    cv2.circle(input, tuple(pointPetitAxe1), 7, [200,200,200], -1)
+    cv2.circle(input, tuple(pointPetitAxe2), 7, [200,200,200], -1)
+
+
+    p=dist(pointGrandAxe1, pointGrandAxe2) #longueur grand axe
+    q=dist(pointPetitAxe1,pointPetitAxe2) #longueur petit axe
+    print("grd {}, petit {}".format(p,q))
+    #On regarde si c'est un carre (petit grand axe = grand axe +-10%):
+    if (p*0.9<=q)and(q>=p*1.1):
+        print "c'est un carre"
+
+    #On regarde si c'est une ellipse
+    c=((p/2.0)**2-(q/2.0)**2)**0.5
+    #calcul des foyers
+    vGrandAxeNorm= np.array(vecGrandAxe)
+    vGrandAxeNorm = vGrandAxeNorm / np.linalg.norm(vGrandAxeNorm)
+    foyer1 = [int(centreGrandAxe[0] + c * vGrandAxeNorm[0]),
+              int(centreGrandAxe[1] + c * vGrandAxeNorm[1])]
+    foyer2 = [int(centreGrandAxe[0] - c * vGrandAxeNorm[0]),
+              int(centreGrandAxe[1] - c * vGrandAxeNorm[1])]
+
+
+
+    cv2.circle(input, tuple(foyer1), 7, [200, 200, 0], -1)
+    cv2.circle(input, tuple(foyer2), 7, [200, 200, 0], -1)
+    #Ellipse si en tout point du contour : distance(foyer1-contour)+distance(foyer2-contour) = cte
+    listeDistanceEllipse = np.array([])
+    for i in range(len(contourUtile)):
+        pt=contourUtile[i][0]
+        d1 = dist(pt,foyer1)
+        d2 = dist(pt,foyer2)
+        dtot=d1+d2
+        listeDistanceEllipse = np.append(listeDistanceEllipse, dtot)
+
+    print("somme dist moyenne std : {}".format(listeDistanceEllipse.std()))
+    if listeDistanceEllipse.std() < 30 :
+        print "!!!!!!!! c'est une ellipse"
+
+    cv2.imshow("point loin", input)
+
+
+
+
 
 
 
